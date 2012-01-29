c9e46746164eea44fd276c7b9309d39674b1e85b
Paul Asmuth <paul@23linesofcode.com>
2012-01-29 22:22:39 +0100

    JaccardProcessor#add_set

--- lib/recommendify/jaccard_processor.rb
+++ lib/recommendify/jaccard_processor.rb
@@ -10,8 +10,28 @@ class Recommendify::JaccardProcessor
     )
   end
 
-  def redis_key
-    [@opts.fetch(:redis_prefix), @opts.fetch(:key)].join(":")
+  def redis_key(append=nil)
+    [@opts.fetch(:redis_prefix), @opts.fetch(:key), append].flatten.compact.join(":")
+  end
+
+  def add_set(set_id, item_ids)
+  	item_ids.each do |item_id|
+  	  item_count_incr(item_id)
+  	end
+  	all_pairs(item_ids).map do |pair| 
+	  i1, i2 = pair.split(":") 
+	  @ccmatrix.incr(i1, i2)
+  	end
+  end
+
+private
+
+  def all_pairs(keys)
+  	keys.map{ |k1| (keys-[k1]).map{ |k2| [k1,k2].sort.join(":") } }.flatten.uniq
+  end
+
+  def item_count_incr(key)
+  	Recommendify.redis.hincrby(redis_key(:items), key, 1)
   end
 
 end
--- spec/jaccard_processor_spec.rb
+++ spec/jaccard_processor_spec.rb
@@ -21,5 +21,38 @@ describe Recommendify::JaccardProcessor do
     @jaccard.ccmatrix.redis_key.should == "recommendify:myjeccard:ccmatrix"
   end
 
+  it "should increment all item counts on addition" do
+    Recommendify.redis.hset("recommendify:myjeccard:items", "bar", 2)
+    @jaccard.add_set("user123", ["foo", "bar"])
+    Recommendify.redis.hget("recommendify:myjeccard:items", "bar").to_i.should == 3
+    Recommendify.redis.hget("recommendify:myjeccard:items", "foo").to_i.should == 1
+  end
+
+  it "should increment all item<->item pairs on set addition" do
+    @jaccard.ccmatrix["bar", "foo"] = 2
+    res = @jaccard.add_set("user123", ["foo", "bar", "fnord"])
+    res.length.should == 3
+    @jaccard.ccmatrix["bar", "foo"].should == 3
+    @jaccard.ccmatrix["foo", "fnord"].should == 1    
+  end
+
+  it "should calculate all item<->item pairs (3)" do
+    res = @jaccard.send(:all_pairs, ["foo", "bar", "fnord"])
+    res.length.should == 3
+    res.should include("bar:foo")
+    res.should include("fnord:foo")
+    res.should include("bar:fnord")
+  end
+
+  it "should calculate all item<->item pairs (6)" do
+    res = @jaccard.send(:all_pairs, ["foo", "bar", "fnord", "blubb"])
+    res.length.should == 6
+    res.should include("bar:foo")
+    res.should include("fnord:foo")
+    res.should include("bar:fnord")
+    res.should include("blubb:foo")
+    res.should include("blubb:fnord")
+    res.should include("bar:blubb")
+  end
 
 end
