ab14976a2245db0550049660c3a58c034765eafd
Paul Asmuth <paul@23linesofcode.com>
2012-01-30 02:03:05 +0100

    todos

--- lib/recommendify/jaccard_processor.rb
+++ lib/recommendify/jaccard_processor.rb
@@ -1,15 +1,18 @@
 class Recommendify::JaccardProcessor < Recommendify::Processor
 
+  # todo: move to coconcurrencymatrix mixin
   attr_reader :ccmatrix
 
   def initialize(opts={})
     super(opts)
+    # todo: move to coconcurrencymatrix mixin
     @ccmatrix = Recommendify::SparseMatrix.new(
       :redis_prefix => @opts.fetch(:redis_prefix),
       :key => [@opts.fetch(:key), :ccmatrix].join(":")
     )
   end
 
+  # todo: move to coconcurrencymatrix mixin
   def add_set(set_id, item_ids)
     item_ids.each do |item_id|
       item_count_incr(item_id)
@@ -24,26 +27,35 @@ class Recommendify::JaccardProcessor < Recommendify::Processor
     calculate_jaccard_cached(item1, item2)
   end
 
+  # optimize: get all item-counts and the cc-row with 2 redis hmgets.
+  # optimize: don't return more than sm.max_neighbors items (truncate set while collecting)
   def similarities_for(item1)
+    # todo: optimize native. execute with own redis conn and write top K to stdout
+    # native_ouput = %x{recommendify_native jaccard "#{redis_key}" "#{item1}"}
+    # return native_output.split("\n").map{ |l| l.split(",") }
     (all_items - [item1]).map do |item2|
       [item2, similarity(item1, item2)]
     end
   end
 
+  # todo: move to coconcurrencymatrix mixin
   def all_items
     Recommendify.redis.hkeys(redis_key(:items))
   end
 
 private
 
+  # todo: move to coconcurrencymatrix mixin
   def all_pairs(keys)
     keys.map{ |k1| (keys-[k1]).map{ |k2| [k1,k2].sort.join(":") } }.flatten.uniq
   end
 
+  # todo: move to coconcurrencymatrix mixin
   def item_count_incr(key)
     Recommendify.redis.hincrby(redis_key(:items), key, 1)
   end
 
+  # todo: move to coconcurrencymatrix mixin
   def item_count(key)
     Recommendify.redis.hget(redis_key(:items), key).to_i
   end
--- lib/recommendify/similarity_matrix.rb
+++ lib/recommendify/similarity_matrix.rb
@@ -4,4 +4,19 @@ class Recommendify::SimilarityMatrix
     puts method
   end
 
+  def process!
+    all_items = processors.map(&:all_items).flatten
+    all_items.each{ |item_id| process_item!(item_id) }
+  end
+
+  # TODO - PSEUDOCODE
+  # def process_item!(item_id)
+  #  top_items = SortedSet.new
+  #  processors.each do |p| 
+  #	  sims = p.similarities_for(item_id)
+  #	  top_items.merge_with_weight(sims, p.weight)
+  #  end
+  #  save_most_similar(item_id, top_items.range(0,max_neighbors))
+  # end
+
 end
