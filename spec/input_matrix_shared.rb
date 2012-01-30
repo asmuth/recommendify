83c67179759b42ba03446f3fdea750529da04597
Paul Asmuth <paul@23linesofcode.com>
2012-01-30 19:42:15 +0100

    cc matrix specs

--- spec/input_matrix_shared.rb
+++ spec/input_matrix_shared.rb
@@ -4,14 +4,24 @@ share_examples_for Recommendify::InputMatrix do
     @matrix.redis_key.should == "recommendify:mymatrix"
   end
 
-  it "should respond to add_set"
+  it "should respond to add_set" do
+    @matrix.respond_to(:add_set).should == true
+  end
 
-  it "should respond to add_single"
+  it "should respond to add_single" do
+    @matrix.respond_to(:add_set).should == true
+  end
 
-  it "should respond to similarity"
+  it "should respond to similarity" do
+    @matrix.respond_to(:add_set).should == true
+  end
 
-  it "should respond to similarities_for"
+  it "should respond to similarities_for" do
+    @matrix.respond_to(:add_set).should == true
+  end
 
-  it "should respond to all_items"
+  it "should respond to all_items" do
+    @matrix.respond_to(:add_set).should == true
+  end
 
 end
