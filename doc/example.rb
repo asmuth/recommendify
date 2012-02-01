54036ea7d31c4c705dda83039aef738ce04b3b5d
Paul Asmuth <paul@23linesofcode.com>
2012-02-01 22:41:23 +0100

    doc/example.rb

--- doc/example.rb
+++ doc/example.rb
@@ -1,22 +1,11 @@
-$: << ::File.expand_path("../lib", __FILE__)
+$: << ::File.expand_path("../../lib", __FILE__)
 require "recommendify"
 require "redis"
 
 # configure redis
 Recommendify.redis = Redis.new
 
-# load some test data
-buckets = Hash.new{ |h,k| h[k]=[] }
-IO.read("/home/paul/talentsuche_visits.csv").split("\n").each do |l|
-  user_id, item_id = l.split(",")
-  next if user_id.length == 0
-  buckets[user_id] << item_id
-end
-
-def item_url(item_id)
-  "http://www.talentsuche.de/u.#{item_id.gsub("profile_", "")}.FNORD.html"
-end
-
+# our recommender class
 class UserRecommender < Recommendify::Base
 
   #max_neighbors 50
@@ -25,11 +14,37 @@ class UserRecommender < Recommendify::Base
     :similarity_func => :jaccard
 end
 
-
 recommender = UserRecommender.new
 
+# load some test data
+buckets = Hash.new{ |h,k| h[k]=[] }
+IO.read("example_data.csv").split("\n").each do |l|
+  user_id, item_id = l.split(",")
+  next if user_id.length == 0
+  buckets[user_id] << item_id
+end
+
+# add the test data to the recommender
+buckets.each do |user_id, items|
+  puts "#{user_id} -> #{items.join(",")}"  
+  recommender.visits.add_set(user_id, items)
+end
+
+# process all items (equivalent to recommender.process!)
+num_items = (all_items = recommender.all_items).size
+all_items.each_with_index do |item_id, n|
+  puts "processing #{item_id} (#{n}/#{num_items})"
+  recommender.process_item!(item_id)
+end
+
+
+# generate a html page
+def item_url(item_id)
+  "http://#{ENV["URL"]}/u.#{item_id.gsub("profile_", "")}.FNORD.html"
+end
+
 out_items = []
-recommender.all_items[0..100].each do |item_id|
+recommender.all_items.shuffle[0..2000].each do |item_id|
   out_recs = recommender.for(item_id)[0..5].map do |rec|
     <<-HTML
       <div class="rec">
@@ -38,6 +53,7 @@ recommender.all_items[0..100].each do |item_id|
       </div>
     HTML
   end
+  next if out_recs.length < 5
   out_items << <<-HTML
     <div class="item">
       <img src="#{item_url(item_id)}" class="item" />
@@ -66,14 +82,3 @@ out = <<-HTML
 HTML
 
 File.open('/tmp/recommendify_out_1.html', "w+"){ |f| f.write(out) }
-
-exit!
-
-
-
-buckets.each do |user_id, items|
-  puts "#{user_id} -> #{items.join(",")}"  
-  recommender.visits.add_set(user_id, items)
-end
-
-recommender.process!
