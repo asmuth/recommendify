c6301ebe7aa234f2f24abbbba847dfa4ec504131
Paul Asmuth <paul@23linesofcode.com>
2012-02-11 23:22:29 +0100

    native: argument validation

--- src/recommendify.c
+++ src/recommendify.c
@@ -4,26 +4,46 @@
 
 #include "version.h" 
 
-int main (int argc, char **argv){
+void print_version(){
+  printf(
+    VERSION_STRING, 
+    VERSION_MAJOR, 
+    VERSION_MINOR, 
+    VERSION_MICRO
+  );
+}
+
+void print_usage(char *bin){
+  printf(USAGE_STRING, bin);
+}
+
+int main(int argc, char **argv){
   redisContext *c;
   int similarityFunc = 0;
   /* redisReply *reply; */
 
-  if(argc < 2) {
-    printf(USAGE_STRING, argv[0]);
+  if(argc < 2){
+    print_usage(argv[0]);
     return 1;
   }
 
   if(!strcmp(argv[1], "--version")){
-    printf(VERSION_STRING, VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
+    print_version();
     return 0;
   }
 
-  if(!strcmp(argv[1], "--jaccard")) similarityFunc = 1;
-  if(!strcmp(argv[1], "--cosine")) similarityFunc = 2;
+  if(!strcmp(argv[1], "--jaccard")) 
+    similarityFunc = 1;
+
+  if(!strcmp(argv[1], "--cosine"))  
+    similarityFunc = 2;
 
   if(!similarityFunc){
     printf("invalid option: %s\n", argv[1]);
+    return 1;
+  } else if(argc != 4){
+    printf("wrong number of arguments\n");
+    print_usage(argv[0]);
     return 1;
   }
 
@@ -37,3 +57,4 @@ int main (int argc, char **argv){
 
   return 0;
 }
+
