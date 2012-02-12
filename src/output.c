9666c474d1d25cad1bcb2b463db951b9360c56af
Mikael Voss <mikael@illdefined.org>
2012-02-12 00:14:45 +0000

    native: No return value for print functions

--- src/output.c
+++ src/output.c
@@ -2,17 +2,15 @@
 
 #include "version.h"
 
-int print_version(){
+void print_version(){
   printf(
     VERSION_STRING, 
     VERSION_MAJOR, 
     VERSION_MINOR, 
     VERSION_MICRO
   );
-  return 0;
 }
 
-int print_usage(char *bin){
+void print_usage(char *bin){
   printf(USAGE_STRING, bin);
-  return 1;
 }
