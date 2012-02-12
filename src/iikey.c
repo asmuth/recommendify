87de5ef763cf0d1ecc1c21461b57e688c5e0fb75
Mikael Voss <mikael@illdefined.org>
2012-02-12 15:04:18 +0000

    native: Avoid snprintf in iikey implementation

--- src/iikey.c
+++ src/iikey.c
@@ -4,21 +4,28 @@
 
 #include "sort.h"
 
+static char *concat(char const *restrict item1, char const *restrict item2) {
+	size_t len1 = strlen(item1);
+	size_t len2 = strlen(item2);
+
+	char *key = malloc(len1 + len2 + 2);
+	if (!key) {
+		perror("malloc");
+		return (char *) 0;
+	}
+
+	memcpy(key, item1, len1);
+	key[len1] = ':';
+	memcpy(key + len1 + 1, item2, len2);
+	key[len1 + 1 + len2] = '\0';
+
+	return key;
+}
+
 char *item_item_key(char const *restrict item1, char const *restrict item2){
-  size_t keylen = strlen(item1) + strlen(item2) + 2;
-  char *key = malloc(keylen * sizeof(char));
-
-  if(!key){
-    perror("malloc");
-    return (char *) 0;
-  }  
-
   // FIXPAUL: make shure this does exactly the same as ruby sort  
-  if(rb_strcmp(item1, item2) <= 0){
-    snprintf(key, keylen, "%s:%s", item1, item2);
-  } else {
-    snprintf(key, keylen, "%s:%s", item2, item1);
-  }
-
-  return key;
+  if (rb_strcmp(item1, item2) <= 0)
+    return concat(item1, item2);
+  else
+    return concat(item2, item1);
 }
