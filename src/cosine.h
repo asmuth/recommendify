cf21ec79cc91f2ba6fffab3c7bb1ff3127da0b18
Mikael Voss <mikael@illdefined.org>
2012-02-12 10:57:33 +0000

    native: Add header with cosine prototypes

---
+++ src/cosine.h
@@ -0,0 +1,8 @@
+#ifndef COSINE_H
+#define COSINE_H
+
+#include "cc_item.h"
+
+void calculate_cosine(char const *restrict item_id, int itemCount, struct cc_item *restrict cc_items, int cc_items_size);
+
+#endif
