0b2d2c32277da5c781a889bc1a8803b0a7be4f5b
Paul Asmuth <paul@paulasmuth.com>
2014-05-05 18:05:01 +0100

    movielens_minhash example stub

--- example/movielens_minhash/movielens_minhash.cc
+++ example/movielens_minhash/movielens_minhash.cc
@@ -22,11 +22,83 @@
  */
 #include <stdlib.h>
 #include <assert.h>
-#include <random>
+#include <fcntl.h>
+#include <unistd.h>
+#include <unordered_map>
 #include "recommendify/minhashrecommender.h"
 
+/**
+ * A minhash/personalized recommender based on the open GroupLens MovieLens 1M
+ * data set. You need {movies,ratings,users}.dat which can be downloaded here:
+ *
+ *   http://grouplens.org/datasets/movielens/
+ *
+ */
 using namespace recommendify;
+void readDatFile(const char* filename,
+    std::function<void(const std::vector<std::string>&)> callback);
+
+/**
+ * Our "database"
+ */
+static std::unordered_map<uint64_t, std::string> movie_names_by_id;
 
 int main() {
-  printf("fnord\n");
+  readDatFile("./movies.dat", [] (const std::vector<std::string>& line) {
+    movie_names_by_id[atoi(line[0].c_str())] = line[1];
+  });
 }
+
+
+/**
+ * Helper code to read the .dat/csv files
+ */
+#define CSV_READ_BUF_SIZE 1024
+void readDatFile(const char* filename,
+    std::function<void(const std::vector<std::string>&)> callback) {
+  std::vector<std::string> line;
+  char buf[CSV_READ_BUF_SIZE];
+  size_t pos = 0;
+  int bytes;
+  int fd = open(filename, O_RDONLY);
+
+  if (fd < 1) {
+    perror("open() failed\n");
+    return;
+  }
+
+  do {
+    bytes = read(fd, buf + pos, CSV_READ_BUF_SIZE - pos);
+
+    if (bytes > 0) {
+      pos += bytes;
+    }
+
+    size_t last = 0;
+
+    for (size_t cur = 0; cur < pos; ++cur) {
+      bool is_separator = (cur > 0 && buf[cur] == ':' && buf[cur - 1] == ':');
+
+      if (buf[cur] == '\n' ||  is_separator) {
+        line.push_back(std::string(buf + last, cur - last - (
+            is_separator ? 1 : 0)));
+
+        last = cur + 1;
+
+        if (buf[cur] == '\n') {
+          callback(line);
+          line.clear();
+        }
+      }
+    }
+
+    if (last > 0) {
+      memcpy(buf, buf + last, pos - last);
+      pos -= last;
+    }
+
+  } while (bytes > 0);
+
+  close(fd);
+}
+
